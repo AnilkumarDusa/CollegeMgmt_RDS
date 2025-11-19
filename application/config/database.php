63182620e93ed500e92dce2d6b7d67480ad504a4
AnilkumarDusa <dusa.anil.kumar@ibm.com>
2025-11-19 15:54:45 +0530

    Update database.php
    Updated database connection details

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'smithadb.mysql.database.azure.com',
-	'username' => 'smithadbuser',
-	'password' => 'Verity@123',
+	'hostname' => 'anildwebapp.azurewebsites.net',
+	'username' => 'anilazurerg',
+	'password' => 'Testing@123',
 	'database' => 'college_mgmt',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
